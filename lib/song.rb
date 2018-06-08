925cce740ec9589ab0cb91668949fc03e8709125
Peter Kim <peterckim96@gmail.com>
2018-06-08 13:31:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   
   def self.new_from_filename(name)
     name.slice!(".mp3")
-    song_array = song_temp.split(" - ")
+    song_array = name.split(" - ")
     song = Song.new
     song.name = song_array[0]
     song.artist_name = song_array[1]
